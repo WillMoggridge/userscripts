ca0d5dd9f733cdac39c1030a4ae5ff42539727af
Will Moggridge <will@moggridge.com>
2017-03-21 13:27:15 +0000

    Add Trello userscript with linting

---
+++ trello/trello-navigate-users.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name Trello - Navigate Users
+// @namespace will@moggridge.com
+// @description Navigate users on Trello with some simple controls.
+// @license LGPL-3.0
+// @version 1
+// @include https://trello.com/b/*
+// @grant none
+// ==/UserScript==
+
+window.addEventListener('load', () => {
+  const nextButton = $('<a class="board-header-btn board-header-btn-filter-indicator">Next User</a>');
+  const userIndicator = $('<a class="board-header-btn"></a>');
+  const prevButton = $('<a class="board-header-btn board-header-btn-filter-indicator">Prev User</a>');
+
+  function resetFilter() {
+    let activeExists = false;
+    do {
+      const active = $('.js-mem-list .js-member-item.active a');
+      active.click();
+      activeExists = (active.length - 1 > 0);
+    } while (activeExists);
+  }
+
+  function changeUser(i) {
+    $('.js-open-card-filter').click();
+    $('.js-show-all-members').click();
+
+    let next = $(`.js-mem-list .js-member-item:nth('${i}') a`);
+    if (next.length) {
+      resetFilter();
+      next = $(`.js-mem-list .js-member-item:nth('${i}') a`);
+      userIndicator.text(next.text());
+      next.click();
+    }
+    return false;
+  }
+
+  function getCurrentUserIndex() {
+    const current = $('.js-mem-list .js-member-item.active:first');
+    if (current.length) {
+      return current.index();
+    }
+    return false;
+  }
+
+  function nextUser() {
+    $('.js-open-card-filter').click();
+    $('.js-show-all-members').click();
+
+    const currentIndex = getCurrentUserIndex();
+    let index = 0;
+    if (currentIndex !== $('.js-mem-list .js-member-item:last').index()) {
+      index = currentIndex + 1;
+    }
+    changeUser(index);
+  }
+
+  function prevUser() {
+    $('.js-open-card-filter').click();
+    $('.js-show-all-members').click();
+
+    const currentIndex = getCurrentUserIndex();
+    let index;
+    if (currentIndex !== false || currentIndex !== 0) {
+      index = currentIndex - 1;
+    } else {
+      index = $('.js-mem-list .js-member-item:last').index();
+    }
+    changeUser(index);
+  }
+
+  nextButton.click(nextUser);
+  prevButton.click(prevUser);
+  $('.board-header-btn.perms-btn').after(userIndicator);
+  $('.board-header-btn.calendar-btn').before(prevButton, nextButton);
+}, false);
